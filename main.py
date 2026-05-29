919af6faeb4b2147cb5e64d1096bafe2ad7f1282
Дедешко Анна <annet.28@mail.ru>
2026-05-30 02:51:16 +0300

    Реализован класс Recipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from pandas.core.dtypes.inference import is_float
 from sympy.physics.units.util import quantity_simplify
-
 
 class Ingredient:
     def __init__(self, name, quantity, unit):
@@ -26,4 +26,53 @@ class Ingredient:
 
     def __eq__(self, other):
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = []
+        for ingredient in ingredients:
+            self.add_ingredient(ingredient)
+
+    def add_ingredient(self, ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффицент должен быть положительным")
+
+        edit_ingredients = []
+        for i in self.ingredients:
+            edit_ingredients.append(Ingredient(i.name, i.quantity * ratio, i.unit))
+
+        return Recipe(self.title, edit_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        s = f"{self.title} \nСписок ингредиентов:"
+        for i in self.ingredients:
+            s += "\n"+str(i)
+        return s
+
+
+
+
+
+
+
+
+
+
+
+
 
