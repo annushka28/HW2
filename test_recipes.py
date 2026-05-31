282d8cf22c69e6b6b67324a06cf54b0cf22c00f0
Дедешко Анна <annet.28@mail.ru>
2026-05-31 21:36:39 +0300

    Написаны тесты для класса Ingredient

--- test_recipes.py
+++ test_recipes.py
@@ -1,1 +1,24 @@
-# здесь будут тесты
+import pytest
+
+from main import Ingredient
+
+def test_ingredient_creation():
+    ingredient = Ingredient("Мука", 500, "г")
+    assert ingredient.name == "Мука"
+    assert ingredient.quantity == 500.0
+    assert ingredient.unit == 'г'
+
+def test_ingredient_str():
+    ingredient = Ingredient("Мука", 500, "г")
+    assert str(ingredient) == "Мука: 500.0 г"
+
+def test_ingredient_eq():
+    ingredient1 = Ingredient("Мука", 500, "г")
+    ingredient2 = Ingredient("Мука", 100, "г")
+    assert ingredient1 == ingredient2
+
+    ingredient2 = Ingredient("Молоко", 500, "г")
+    assert ingredient1 != ingredient2
+
+    ingredient2 = Ingredient("Мука", 500, "кг")
+    assert ingredient1 != ingredient2
